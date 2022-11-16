f1404a39d313d5710956e53e883a1e413612537d
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2022-11-16 09:15:29 +0100

    strisnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 12:33:04 by gmillon           #+#    #+#             */
-/*   Updated: 2022/07/13 06:06:06 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/11/16 09:14:58 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,11 @@ int	ft_isalnum(int c)
 	if ((ft_isupper(c) || ft_islower(c) || (c >= 48 && c <= 57)))
 		return (1);
 	return (0);
+}
+
+int	ft_str_is_num(char *s)
+{
+	while(s && *s >= 48 && *s <= 57)
+		s++;
+	return (*s);
 }
